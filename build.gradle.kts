2936fe9c2240a4a52cd05cc92215a2bfba4ea4fc
Anton Musichin <musichin@users.noreply.github.com>
2021-11-05 19:28:16 +0100

    adds code coverage report

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 buildscript {
     repositories {
         google()
@@ -10,9 +8,6 @@ buildscript {
         classpath(kotlin("gradle-plugin", version = "1.5.31"))
         classpath("com.android.tools.build:gradle:7.0.3")
         classpath("com.vanniktech:gradle-maven-publish-plugin:0.18.0")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle.kts files
     }
 }
 
